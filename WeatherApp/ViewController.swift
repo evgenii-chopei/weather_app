963e2b71bc7df5f023bf1b571f3937dc028399b5
Evgenii Chopei <evgeniichopei@evgeniis-macbook-pro.local>
2018-09-27 16:22:47 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Evgenii Chopei on 9/27/18.
+//  Copyright © 2018 Evgenii Chopei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+    }
+
+
+}
+
